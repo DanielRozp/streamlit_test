22020140870471457797704d71a6c9f846bfc56e
DanielRozp <51082178+DanielRozp@users.noreply.github.com>
2023-09-25 17:57:06 +0200

    Update str.py

--- str.py
+++ str.py
@@ -16,8 +16,8 @@ def main():
   stores = ['Badalona', 'Toledo', 'Alcalá de henares']
   query = st.selectbox('Store name', stores, help='Type the name of the store you want to compare and retrieve its mirror store.')
 
-  if query == 'Badalona':
-    diagnosis = 'For Badalona the mirror sotres are: Alcobendas, Toledo, Huercal with 90% of similarity'
+  if st.selectbox('Badalona'):
+    st.write('Hola')
   
 
 if __name__ == '__main__':
