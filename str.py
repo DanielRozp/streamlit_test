b528c692f359cdb47ac555e8e6cbb07add38704c
DanielRozp <51082178+DanielRozp@users.noreply.github.com>
2023-09-25 15:28:13 +0200

    Update str.py

--- str.py
+++ str.py
@@ -1,1 +1,11 @@
 
+import streamlit as st
+
+def main():
+  #giving a title
+  st.title('Mirror Store Prediction Web APP')
+
+  # getting the input data from the user
+  store = st.text_input('Name of Store')
+  
+  
