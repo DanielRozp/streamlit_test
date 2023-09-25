3b52dc0e89b3ebe0e861e6b80bf056f3fccceede
DanielRozp <51082178+DanielRozp@users.noreply.github.com>
2023-09-25 17:51:01 +0200

    Update str.py

--- str.py
+++ str.py
@@ -14,6 +14,8 @@ def main():
   # creating a button for predictions
   stores = ['Badalona', 'Toledo', 'Alcalá de henares']
   st.selectbox('Store name', stores, help='Type the name of the store you want to compare and retrieve its mirror store.')
+  if st.button('Test Result'):
+    diagnosis = 'For '+store+' the mirror sotres are: Alcobendas, Toledo, Huercal with 90% of similarity'
 
 if __name__ == '__main__':
   main()
